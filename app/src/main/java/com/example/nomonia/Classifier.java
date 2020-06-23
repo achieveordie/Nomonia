f17fe9e79f214551cb64efd4cebf5348676bb415
Sagar Mishra <54197164+achieveordie@users.noreply.github.com>
2020-06-23 17:05:46 +0530

    Contains gradle files

---
+++ app/src/main/java/com/example/nomonia/Classifier.java
@@ -0,0 +1,143 @@
+package com.example.nomonia;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.media.Image;
+
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.Interpreter;
+import org.tensorflow.lite.gpu.GpuDelegate;
+import org.tensorflow.lite.support.common.FileUtil;
+import org.tensorflow.lite.support.common.TensorProcessor;
+import org.tensorflow.lite.support.image.ImageProcessor;
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.image.ops.ResizeWithCropOrPadOp;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
+
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+
+
+public class Classifier {
+
+    public enum Device {
+        CPU,
+        GPU
+    }
+
+    private MappedByteBuffer tfliteModel;
+    private int imageSizeX;
+    private int imageSizeY;
+
+    private GpuDelegate gpuDelegate = null;
+
+    private Interpreter tflite;
+
+    private final Interpreter.Options tfliteOptions = new Interpreter.Options();
+
+    private TensorImage inputImageBuffer;
+    private TensorBuffer outputProbabilitybuffer;
+    private TensorProcessor probabilityProcessor;
+
+    private String getModelpath() {
+        return "model.tflite";
+    }
+
+    public static class Recognition {
+        private String id;
+        private Float confidence;
+
+        public Recognition(
+                String id, Float confidence
+        ){
+            this.id = id;
+            this.confidence = confidence;
+        }
+
+        public String getId(){return id;}
+
+        public Float getConfidence(){return confidence;}
+
+        @Override
+        public String toString(){
+            String resultString = "";
+            if (id != null) {
+                resultString += "[" + id + "]";
+            }
+            if (confidence != null){
+                resultString += String.format("(%.3f%%) ", confidence*100.0f);
+            }
+            return resultString;
+        }
+    }
+
+    protected Classifier(Activity activity, Device device, int numThreads)
+        throws IOException {
+        tfliteModel = FileUtil.loadMappedFile(activity, getModelpath());
+        switch (device) {
+            case GPU:
+                gpuDelegate = new GpuDelegate();
+                tfliteOptions.addDelegate(gpuDelegate);
+                break;
+            case CPU:
+                break;
+        }
+        tfliteOptions.setNumThreads(numThreads);
+
+        tflite = new Interpreter(tfliteModel, tfliteOptions);
+
+        int imageTensorIndex = 0;
+        int [] imageShape = tflite.getInputTensor(imageTensorIndex).shape();
+        imageSizeX = imageShape[2];
+        imageSizeY = imageShape[1];
+        DataType imageDataType = tflite.getInputTensor(imageTensorIndex).dataType();
+        int probabilityTensorIndex = 0;
+        int [] probabilityShape =
+                tflite.getOutputTensor(probabilityTensorIndex).shape();
+        DataType probabilityDataType = tflite.getOutputTensor(probabilityTensorIndex).dataType();
+
+        inputImageBuffer = new TensorImage(imageDataType);
+
+        outputProbabilitybuffer = TensorBuffer.createFixedSize(probabilityShape, probabilityDataType);
+
+        probabilityProcessor = new TensorProcessor.Builder().build();
+
+    }
+
+    public Float recognizeImage(final Bitmap bitmap){
+        inputImageBuffer = loadImage(bitmap);
+        tflite.run(inputImageBuffer.getBuffer(), outputProbabilitybuffer.getBuffer().rewind());
+        float[] predictions = outputProbabilitybuffer.getFloatArray();
+        return predictions[1];
+    }
+
+    public void close() {
+        if (tflite != null) {
+            tflite.close();
+            tflite = null;
+        }
+        if (gpuDelegate != null){
+            gpuDelegate.close();
+            gpuDelegate = null;
+        }
+
+        tfliteModel = null;
+    }
+
+    public int getImageSizeX() {return imageSizeX;}
+
+    public int getImageSizeY() {return imageSizeY;}
+
+    private TensorImage loadImage(Bitmap bitmap){
+        inputImageBuffer.load(bitmap);
+        int cropSize = 100;
+        ImageProcessor imageProcessor =
+                new ImageProcessor.Builder()
+                    .add(new ResizeWithCropOrPadOp(cropSize, cropSize))
+                    .build();
+        return imageProcessor.process(inputImageBuffer);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/nomonia/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.nomonia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.net.URI;
+
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    Button search_button;
+    Button reset_button;
+    private static final int PICK_IMAGE = 100;
+    Uri imageUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = (ImageView)findViewById(R.id.image_box);
+        search_button = (Button)findViewById(R.id.button_search);
+        reset_button = (Button)findViewById(R.id.button_reset);
+
+        search_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openGallery();
+            }
+        });
+
+        reset_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                resetImage();
+            }
+        });
+    }
+
+    private void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+
+    private void resetImage(){
+        imageView.setBackground(null);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+        }
+    }
+}
